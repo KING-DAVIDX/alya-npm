24c80f52c369e09dbe89cc3702c08a344d4e8a89
KING DAVID <xking6030@gmail.com>
2025-08-21 09:29:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const veo3 = require('./lib/veo3');
 const utils = require('./lib/utils');
+const sticker = require('./lib/sticker');
 
 module.exports = {
   veo3,
-  utils
+  utils,
+  sticker
 };
